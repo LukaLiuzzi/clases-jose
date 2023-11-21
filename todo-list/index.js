915c486d6f5428b71c51cc7d10698e809cc079c6
Luka <lukaliuzzidev@gmail.com>
2023-11-21 16:59:18 -0300

    add todolist

---
+++ todo-list/index.js
@@ -0,0 +1,7 @@
+// TODO LIST
+/**
+ * Cada tarea debe tener un id, una tarea, una categoria, una fecha de creacion, un estado (completado o pendiente) y una fecha de completado.
+ * Las categorias deben tener un id y un nombre y deben estar preestablecidas.
+ * Las tareas deben poder ser creadas, editadas, eliminadas y completadas.
+ * Las tareas deben poder ser filtradas por categoria y por estado.
+ */
